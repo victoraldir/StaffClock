cec615200faabf0a24471aa110cf4512a23687b8
victoraldir <vhaldir@gmail.com>
2018-07-16 23:00:43 -0300

    Initial commit

---
+++ app/src/main/java/com/quartzo/staffclock/utils/DateTimeUtils.java
@@ -0,0 +1,37 @@
+package com.quartzo.staffclock.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
+
+public class DateTimeUtils {
+
+    private static final String TIME = "(?m)^(\\d\\d:\\d\\d)";
+    private static final String DATE = "(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)";
+
+    private static Pattern DATE_PATTERN = Pattern.compile(DATE);
+    private static Pattern TIME_PATTERN = Pattern.compile(TIME);
+
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+    public static boolean matchDate(String date) {
+        return DATE_PATTERN.matcher(date).matches();
+    }
+
+    public static boolean matchTime(String time) {
+        return TIME_PATTERN.matcher(time).matches();
+    }
+
+
+    public static Date parseToDate(String date){
+        try {
+            return simpleDateFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
---
+++ app/src/test/java/com/quartzo/staffclock/utils/DateTimeUtilsTest.java
@@ -0,0 +1,39 @@
+package com.quartzo.staffclock.utils;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DateTimeUtilsTest {
+
+    @Test
+    public void shouldMatchDate(){
+
+        String dateString = "12/12/1990";
+
+        Assert.assertTrue(DateTimeUtils.matchDate(dateString));
+
+    }
+
+    @Test
+    public void shouldMatchTime(){
+
+        String timeString = "13:21";
+
+        Assert.assertTrue(DateTimeUtils.matchTime(timeString));
+
+    }
+
+    @Test
+    public void shouldParseStringToDate(){
+
+        String dateTimeString = "12/12/2018 13:21";
+
+        System.out.print(DateTimeUtils.parseToDate(dateTimeString));
+
+        Assert.assertNotNull(DateTimeUtils.parseToDate(dateTimeString));
+
+    }
+}
